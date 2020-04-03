77c0a103b4b736d339b427063d3ba60fc214aab4
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-04-02 19:51:10 -0500

    chages for new SOS website

--- common.py
+++ common.py
@@ -25,8 +25,20 @@ dc = DocumentCloud(username=os.environ['DOCUMENT_CLOUD_USERNAME'],
                    password=os.environ['DOCUMENT_CLOUD_PW'])
 
 
-muh_headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-
+muh_headers = {"Content-Type": "application/json; charset=utf-8",
+             "Pragma": "no-cache",
+             "Accept": "*/*",
+             "Accept-Language": "en-us",
+             "Accept-Encoding": "gzip, deflate, br",
+             "Cache-Control": "no-cache",
+             "Host": "www.sos.ms.gov",
+             "Origin": "https://www.sos.ms.gov",
+             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
+             "Referer": "https://www.sos.ms.gov/content/executiveorders/default.aspx",
+             "Content-Length": "0",
+             "Connection": "keep-alive",
+             "Cookie": "f5_cspm=1234; _ga=GA1.2.1800542741.1584480254; _gat=1; _gid=GA1.2.639081259.1585870866; f5avraaaaaaaaaaaaaaaa_session_=NOKMMGKKIAGOJCILJLKFGAFONADMLGLLFPLFFNCONMMEHKMOHAHFCHKAAHHIDFALMPKDDNOLFLPJKNEFPKPAFOOCCHHPBMOKANAGKCEHOAMFKIGEMNFOFPPGGHOKMPEP; BIGipServerpl_sos_web_server_ext_https=rd1o00000000000000000000ffff0a0d3b0bo443; BIGipServerpl_msi_prod_https=rd1o00000000000000000000ffff0a0df71fo443",
+             "X-Requested-With": "XMLHttpRequest"}
 
 tw = Twython(os.environ['TWITTER_APP_KEY'], os.environ['TWITTER_APP_SECRET'],
              os.environ['TWITTER_OAUTH_TOKEN'], os.environ['TWITTER_OAUTH_TOKEN_SECRET'])
--- sos_scraper.py
+++ sos_scraper.py
@@ -4,7 +4,6 @@ import os
 import time
 from io import BytesIO
 import requests
-from bs4 import BeautifulSoup
 from common import airtab_sos as airtab, dc, tw, muh_headers, wrap_from_module
 
 
@@ -13,57 +12,53 @@ wrap_it_up = wrap_from_module('sos_scraper.py')
 
 def scrape_exec_orders():
     """This function does blah blah."""
-    url, t0, new, total = 'https://www.sos.ms.gov/Education-Publications/Pages/Executive-Orders.aspx', time.time(), 0, 0
-    t0 = time.time()
-    r = requests.get(url, headers=muh_headers)
-    soup = BeautifulSoup(r.text, "html.parser").find('table', class_='table-striped')
-    rows = soup.find_all("tr")
-    for row in rows:
-        cells = row.find_all("td")
-        if len(cells) == 3:
-            total += 1
-            this_dict = {}
-            if cells[0].string:
-                this_dict["order_number"] = cells[0].string.strip()
-                m = airtab.match("order_number", this_dict["order_number"])
-                if not m:
-                    new += 1
-                    # There's a new Exec Order!
-                    this_dict["order_url"] = f"https://www.sos.ms.gov{cells[1].a.get('href')}"
-                    this_dict["url_description"] = cells[1].get_text(
-                        "\n", strip=True)
-                    if cells[2].string:
-                        this_dict["date_of_order"] = cells[2].string.strip()
-                    # upload to documnentcloud
-                    fn = "Executive Order No. " + this_dict["order_number"]
-                    obj = dc.documents.upload(
-                        this_dict["order_url"], title=fn, source='MS SOS', access="public", data={'type': 'EO'})
-                    obj = dc.documents.get(obj.id)
-                    while obj.access != "public":
-                        time.sleep(7)
-                        obj = dc.documents.get(obj.id)
-                    this_dict["dc_id"] = obj.id
-                    this_dict["dc_title"] = obj.title
-                    this_dict["dc_access"] = obj.access
-                    this_dict["dc_pages"] = obj.pages
-                    full_text = obj.full_text.decode("utf-8")
-                    this_dict["dc_full_text"] = os.linesep.join(
-                        [s for s in full_text.splitlines() if s])
-                    airtab.insert(this_dict, typecast=True)
-                    media_ids = []
-                    image_list = obj.normal_image_url_list[:4]
-                    for image in image_list:
-                        res = requests.get(image)
-                        res.raise_for_status()
-                        uploadable = BytesIO(res.content)
-                        response = tw.upload_media(media=uploadable)
-                        media_ids.append(response["media_id"])
-                    status = (
-                        "The Sec. of State has added an executive order to its website.\n"
-                        f"On {this_dict['date_of_order']}, Gov. Reeves issued {this_dict['dc_title']}.\n"
-                        f"{this_dict['order_url']}"
-                    )
-                    tw.update_status(status=status, media_ids=media_ids)
+    t0, new, total = time.time(), 0, 0
+    url = 'https://www.sos.ms.gov/content/executiveorders/EOFunctions.asmx/ListExecutiveOrders'
+    r = requests.post(url, headers=muh_headers)
+    annoying_blob = r.json()['d']
+    rows = annoying_blob.replace('~~', '').split('^')
+    for row in rows[:10]:
+        cells = row.split('|')
+        total = len(cells)
+        this_dict = {}
+        this_dict['order_number'] = cells[0].strip()
+        m = airtab.match('order_number', this_dict['order_number'])
+        if not m:
+            new += 1
+            # There's a new Exec Order!
+            this_dict['order_url'] = f"https://www.sos.ms.gov/content/executiveorders/ExecutiveOrders/{cells[4]}.pdf"
+            this_dict['url_description'] = cells[5]
+            this_dict['date_of_order'] = cells[3]
+            # upload to documnentcloud
+            fn = 'Executive Order No. ' + this_dict['order_number']
+            obj = dc.documents.upload(
+                this_dict['order_url'], title=fn, source='MS SOS', access='public', data={'type': 'EO'})
+            obj = dc.documents.get(obj.id)
+            while obj.access != 'public':
+                time.sleep(7)
+                obj = dc.documents.get(obj.id)
+            this_dict['dc_id'] = obj.id
+            this_dict['dc_title'] = obj.title
+            this_dict['dc_access'] = obj.access
+            this_dict['dc_pages'] = obj.pages
+            full_text = obj.full_text.decode('utf-8')
+            this_dict['dc_full_text'] = os.linesep.join(
+                [s for s in full_text.splitlines() if s])
+            airtab.insert(this_dict, typecast=True)
+            media_ids = []
+            image_list = obj.normal_image_url_list[:4]
+            for image in image_list:
+                res = requests.get(image)
+                res.raise_for_status()
+                uploadable = BytesIO(res.content)
+                response = tw.upload_media(media=uploadable)
+                media_ids.append(response['media_id'])
+            status = (
+                'The Sec. of State has added an executive order to its website.\n'
+                f"On {this_dict['date_of_order']}, Gov. Reeves issued {this_dict['dc_title']}.\n"
+                f"{this_dict['order_url']}"
+            )
+            tw.update_status(status=status, media_ids=media_ids)
     wrap_it_up(t0=t0, new=new, total=total, function='scrape_exec_orders')
 
 
@@ -71,5 +66,5 @@ def main():
     scrape_exec_orders()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

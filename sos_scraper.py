58e3fc8b393c29e0f8feed8095d2b64899ede828
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-04-10 17:25:52 -0500

    Update sos_scraper.py

--- sos_scraper.py
+++ sos_scraper.py
@@ -35,8 +35,8 @@ def scrape_exec_orders():
                 this_dict['order_url'], title=fn, source='MS SOS', access='public', data={'type': 'EO'})
             obj = dc.documents.get(obj.id)
             loops = 1
-            while obj.access != 'public' and loops < 4:
-                time.sleep(7)
+            while obj.access != 'public' and loops < 30:
+                time.sleep(6)
                 obj = dc.documents.get(obj.id)
                 loops += 1
             this_dict['dc_id'] = obj.id
@@ -50,6 +50,54 @@ def scrape_exec_orders():
             except NotImplementedError as err:
                 print('problem uploading to documentcloud: ', err)
             airtab.insert(this_dict, typecast=True)
+            status = (
+                'The Sec. of State has added an executive order to its website.\n'
+                f"On {this_dict['date_of_order']}, Gov. Reeves issued {this_dict['dc_title']}.\n"
+                f"{this_dict['order_url']}"
+            )
+            try:
+                media_ids = []
+                image_list = obj.normal_image_url_list[:4]
+                for image in image_list:
+                    res = requests.get(image)
+                    res.raise_for_status()
+                    uploadable = BytesIO(res.content)
+                    response = tw.upload_media(media=uploadable)
+                    media_ids.append(response['media_id'])
+                tw.update_status(status=status, media_ids=media_ids)
+            except requests.exceptions.HTTPError as err:
+                print('problem accessing documentcloud page images: ', err)
+                tw.update_status(status=status)
+    wrap_it_up(t0=t0, new=new, total=total, function='scrape_exec_orders')
+
+
+def scrape_exec_orders_v2():
+    """This function does blah blah."""
+    t0, new, total = time.time(), 0, 0
+    orders = airtab.get_all(view='not tweeted')
+    for order in orders:
+        this_dict = {'dc_id': order['fields']['dc_id']}
+        obj = dc.documents.get(this_dict['dc_id'])
+        loops = 1
+        while obj.access != 'public' and loops < 30:
+            time.sleep(6)
+            obj = dc.documents.get(obj.id)
+            loops += 1
+        try:
+            this_dict['dc_access'] = obj.access
+            this_dict['dc_pages'] = obj.pages
+            full_text = obj.full_text.decode('utf-8')
+            this_dict['dc_full_text'] = os.linesep.join(
+                [s for s in full_text.splitlines() if s])
+        except NotImplementedError as err:
+            print('problem uploading to documentcloud: ', err)
+        airtab.update(order['id'], this_dict, typecast=True)
+        status = (
+            'The Sec. of State has added an executive order to its website.\n'
+            f"On {order['fields']['date_of_order_string']}, Gov. Reeves issued {order['fields']['dc_title']}.\n"
+            f"{order['fields']['order_url']}"
+        )
+        try:
             media_ids = []
             image_list = obj.normal_image_url_list[:4]
             for image in image_list:
@@ -58,18 +106,16 @@ def scrape_exec_orders():
                 uploadable = BytesIO(res.content)
                 response = tw.upload_media(media=uploadable)
                 media_ids.append(response['media_id'])
-            status = (
-                'The Sec. of State has added an executive order to its website.\n'
-                f"On {this_dict['date_of_order']}, Gov. Reeves issued {this_dict['dc_title']}.\n"
-                f"{this_dict['order_url']}"
-            )
             tw.update_status(status=status, media_ids=media_ids)
+        except requests.exceptions.HTTPError as err:
+            print('problem accessing documentcloud page images: ', err)
+            tw.update_status(status=status)
     wrap_it_up(t0=t0, new=new, total=total, function='scrape_exec_orders')
 
 
 def main():
     scrape_exec_orders()
-
+    scrape_exec_orders_v2()
 
 if __name__ == '__main__':
     main()

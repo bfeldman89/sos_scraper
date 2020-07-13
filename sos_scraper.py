4bcb03f82d7e8742ed341313c85b70db87a24b10
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-07-13 18:16:35 -0500

    Update sos_scraper.py

--- sos_scraper.py
+++ sos_scraper.py
@@ -1,15 +1,25 @@
 # !/usr/bin/env python
 """This module does blah blah."""
-import os
 import time
 from io import BytesIO
 import requests
 from documentcloud import exceptions
-
 from common import airtab_sos as airtab, dc, tw, muh_headers, wrap_from_module
 
-
 wrap_it_up = wrap_from_module('sos_scraper.py')
+
+
+def get_images(dc_id):
+    media_ids = []
+    obj = dc.documents.get(dc_id)
+    image_list = obj.normal_image_url_list[:4]
+    for image in image_list:
+        res = requests.get(image)
+        res.raise_for_status()
+        uploadable = BytesIO(res.content)
+        response = tw.upload_media(media=uploadable)
+        media_ids.append(response['media_id'])
+    return media_ids
 
 
 def scrape_exec_orders():
@@ -33,19 +43,24 @@ def scrape_exec_orders():
             this_dict['date_of_order'] = cells[3]
             # upload to documnentcloud
             obj = dc.documents.upload(this_dict['order_url'])
-            while obj.access not in {"public", "success"}:
-                print(obj.access)
-                try:
-                    obj.access = "public"
-                    obj.put()
-                except exceptions.APIError as err:
-                    print(err)
-                    time.sleep(5)
-                    obj = dc.documents.get(obj.id)
-            obj.title = 'Executive Order No. ' + this_dict['order_number']
-            obj.source = 'MS SOS'
+            while obj.status != "success":
+                time.sleep(5)
+                obj = dc.documents.get(obj.id)
+            # while obj.access != "public":
+            #     time.sleep(5)
+            #     try:
+            #         obj.access = "public"
+            #         obj.data = {'type': 'EO'}
+            #         obj.put()
+            #     except exceptions.APIError as err:
+            #         print(err)
+            #     obj = dc.documents.get(obj.id)
+            obj.access = "public"
             obj.data = {'type': 'EO'}
             obj.put()
+            obj.title = 'Executive Order No. ' + this_dict['order_number']
+            obj.source = 'MS SOS'
+
             this_dict['dc_id'] = str(obj.id)
             this_dict['dc_title'] = obj.title
             this_dict['dc_access'] = obj.access
@@ -56,67 +71,13 @@ def scrape_exec_orders():
                 f"On {this_dict['date_of_order']}, Gov. Reeves issued {this_dict['dc_title']}.\n"
                 f"{this_dict['order_url']}"
             )
-            try:
-                media_ids = []
-                image_list = obj.normal_image_url_list[:4]
-                for image in image_list:
-                    res = requests.get(image)
-                    res.raise_for_status()
-                    uploadable = BytesIO(res.content)
-                    response = tw.upload_media(media=uploadable)
-                    media_ids.append(response['media_id'])
-                tw.update_status(status=status, media_ids=media_ids)
-            except requests.exceptions.HTTPError as err:
-                print('problem accessing documentcloud page images: ', err)
-                tw.update_status(status=status)
-    wrap_it_up(t0=t0, new=new, total=total, function='scrape_exec_orders')
-
-
-def scrape_exec_orders_v2():
-    """This function does blah blah."""
-    t0, new, total = time.time(), 0, 0
-    orders = airtab.get_all(view='not tweeted')
-    for order in orders:
-        this_dict = {'dc_id': order['fields']['dc_id']}
-        obj = dc.documents.get(this_dict['dc_id'])
-        loops = 1
-        while obj.access != 'public' and loops < 30:
-            time.sleep(6)
-            obj = dc.documents.get(obj.id)
-            loops += 1
-        try:
-            this_dict['dc_access'] = obj.access
-            this_dict['dc_pages'] = obj.pages
-            full_text = obj.full_text
-            this_dict['dc_full_text'] = os.linesep.join(
-                [s for s in full_text.splitlines() if s])
-        except NotImplementedError as err:
-            print('problem uploading to documentcloud: ', err)
-        airtab.update(order['id'], this_dict, typecast=True)
-        status = (
-            'The Sec. of State has added an executive order to its website.\n'
-            f"On {order['fields']['date_of_order_string']}, Gov. Reeves issued {order['fields']['dc_title']}.\n"
-            f"{order['fields']['order_url']}"
-        )
-        try:
-            media_ids = []
-            image_list = obj.normal_image_url_list[:4]
-            for image in image_list:
-                res = requests.get(image)
-                res.raise_for_status()
-                uploadable = BytesIO(res.content)
-                response = tw.upload_media(media=uploadable)
-                media_ids.append(response['media_id'])
+            media_ids = get_images(obj.id)
             tw.update_status(status=status, media_ids=media_ids)
-        except requests.exceptions.HTTPError as err:
-            print('problem accessing documentcloud page images: ', err)
-            tw.update_status(status=status)
     wrap_it_up(t0=t0, new=new, total=total, function='scrape_exec_orders')
 
 
 def main():
     scrape_exec_orders()
-    scrape_exec_orders_v2()
 
 
 if __name__ == '__main__':

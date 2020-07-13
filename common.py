c8ca5e90e8102c8ff861428282fecead44569991
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-07-13 08:25:08 -0500

    Update common.py

--- common.py
+++ common.py
@@ -21,26 +21,24 @@ airtab_tweets = Airtable(base_key=os.environ['botfeldman89_db'],
                          api_key=os.environ['AIRTABLE_API_KEY'])
 
 
-# dc = DocumentCloud(username=os.environ['DOCUMENT_CLOUD_USERNAME'],
-#                    password=os.environ['DOCUMENT_CLOUD_PW'])
 
 dc = DocumentCloud(username=os.environ['MUCKROCK_USERNAME'],
                    password=os.environ['MUCKROCK_PW'])
 
 muh_headers = {"Content-Type": "application/json; charset=utf-8",
-             "Pragma": "no-cache",
-             "Accept": "*/*",
-             "Accept-Language": "en-us",
-             "Accept-Encoding": "gzip, deflate, br",
-             "Cache-Control": "no-cache",
-             "Host": "www.sos.ms.gov",
-             "Origin": "https://www.sos.ms.gov",
-             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
-             "Referer": "https://www.sos.ms.gov/content/executiveorders/default.aspx",
-             "Content-Length": "0",
-             "Connection": "keep-alive",
-             "Cookie": "f5_cspm=1234; _ga=GA1.2.1800542741.1584480254; _gat=1; _gid=GA1.2.639081259.1585870866; f5avraaaaaaaaaaaaaaaa_session_=NOKMMGKKIAGOJCILJLKFGAFONADMLGLLFPLFFNCONMMEHKMOHAHFCHKAAHHIDFALMPKDDNOLFLPJKNEFPKPAFOOCCHHPBMOKANAGKCEHOAMFKIGEMNFOFPPGGHOKMPEP; BIGipServerpl_sos_web_server_ext_https=rd1o00000000000000000000ffff0a0d3b0bo443; BIGipServerpl_msi_prod_https=rd1o00000000000000000000ffff0a0df71fo443",
-             "X-Requested-With": "XMLHttpRequest"}
+               "Pragma": "no-cache",
+               "Accept": "*/*",
+               "Accept-Language": "en-us",
+               "Accept-Encoding": "gzip, deflate, br",
+               "Cache-Control": "no-cache",
+               "Host": "www.sos.ms.gov",
+               "Origin": "https://www.sos.ms.gov",
+               "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
+               "Referer": "https://www.sos.ms.gov/content/executiveorders/default.aspx",
+               "Content-Length": "0",
+               "Connection": "keep-alive",
+               "Cookie": "f5_cspm=1234; _ga=GA1.2.1800542741.1584480254; _gat=1; _gid=GA1.2.639081259.1585870866; f5avraaaaaaaaaaaaaaaa_session_=NOKMMGKKIAGOJCILJLKFGAFONADMLGLLFPLFFNCONMMEHKMOHAHFCHKAAHHIDFALMPKDDNOLFLPJKNEFPKPAFOOCCHHPBMOKANAGKCEHOAMFKIGEMNFOFPPGGHOKMPEP; BIGipServerpl_sos_web_server_ext_https=rd1o00000000000000000000ffff0a0d3b0bo443; BIGipServerpl_msi_prod_https=rd1o00000000000000000000ffff0a0df71fo443",
+               "X-Requested-With": "XMLHttpRequest"}
 
 tw = Twython(os.environ['TWITTER_APP_KEY'], os.environ['TWITTER_APP_SECRET'],
              os.environ['TWITTER_OAUTH_TOKEN'], os.environ['TWITTER_OAUTH_TOKEN_SECRET'])
